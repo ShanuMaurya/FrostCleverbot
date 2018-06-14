7e41e69387267b25b58e5b8a7ebba217d820b825
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-06-14 23:12:06 +0530

    Update frost.py

--- frost.py
+++ frost.py
@@ -1,26 +1,20 @@
-import discord
-import asyncio
-import requests
-import json
+const DiscordRPC = require("discord-rpc");
 
-client = discord.Client()
-user = 'GiVEA3k2vEKSJX0m'
-key = 'ye47ohHERGhVcNw1FjcfnVpPK6sW1bZl'
+const ClientId = '456423224960352268';
 
-@client.event
-async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
-    await client.change_presence(game=discord.Game(name='ZeØ On Discord'))
+DiscordRPC.register(ClientId);
 
-@client.event
-async def on_message(message):
-    if not message.author.bot and (message.server == None or client.user in message.mentions):
-        await client.send_typing(message.channel)
-        txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
-        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'frost', 'text':txt}).text)
-        if r['status'] == 'success':
-            await client.send_message(message.channel, r['response'] )
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
 
-print('Starting...')
-requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'frost'})
-client.run('NDM3MjE5ODgwMTc4MDI0NDQ4.DgGgfg.SAIeFsUMvLDayX97PG7m7PBPL7E')
+rpc.on('ready', () => {
+  console.log("ready");
+  rpc.setActivity({
+    details: `Modification of Pεтяσℓ Mαятιηι`,
+    state: 'Working on Brain.Pεтяσl',
+    largeImageKey: 'hr2',
+    largeImageText: 'Modification of Petrol ',
+    smallImageKey: 'hr',
+    smallImageText: 'not done yet',
+    instance: true,
+  });
+});
