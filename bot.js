9d17cbaa27b79ece772877c9e3bb1b0eb946d00e
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-09-05 12:47:41 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+
+const DiscordRPC = require("discord-rpc");
+
+const ClientId = '456423224960352268';
+
+DiscordRPC.register(ClientId);
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+rpc.on('ready', () => {
+  console.log("ready");
+  rpc.setActivity({
+    details: `Modification of Pεтяσℓ Mαятιηι`,
+    state: 'Working on Brain.Pεтяσl',
+    largeImageKey: 'hr2',
+    largeImageText: 'Modification of Petrol',
+    smallImageKey: 'hr',
+    smallImageText: 'not done yet',
+    instance: false,
+  });
+});
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
